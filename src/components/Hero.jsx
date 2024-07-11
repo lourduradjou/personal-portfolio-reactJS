16babdca618a581d18b3d3183375059c8933d2a3
Lourdu Radjou <rajlourdu15@gmail.com>
2024-07-11 20:57:09 +0530

    hero component was done with responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,35 +4,29 @@ import myPhoto from '../img/myPhoto.jpeg'
 
 const Hero = () => {
 	return (
-		<div className='py-4 w-auto word-spacing-wide mx-auto text-black font-medium text-center bg-[#acacac] bg-opacity-80 md:rounded-md md:flex md:justify-around md:items-center tracking-[.1em]  font-outfit'>
-			<div className='w-full text-2xl mx-auto px-8 '>
-				<img
-					src={myPhoto}
-					className='m-4 w-[200px] h-auto mx-auto rounded-e-full'
-					alt='lourdu-radjou-photo'
-				/>
-				{data.about1.split(',').map((line, index) => (
-					<p
-						key={index}
-						className={` text-center border-b-2 border-b-slate-300 mb-4 ${
-							line === 'Lourdu Radjou'
-								? 'bg-gradient bg-clip-text text-transparent font-medium'
-								: ''
-						}`}
-					>
-						{line}
-					</p>
-				))}
-			</div>
-			<div className='border-l-4 border-[#373737] h-full w-full py-20 text-2xl mx-auto px-8 '>
-				{data.about2.split(',').map((line, index) => (
-					<p
-						key={index}
-						className='text-center border-b-2 border-b-red-400 mb-4'
-					>
-						{line}
-					</p>
-				))}
+		<div
+			id='Home'
+			className='w-full h-screen flex justify-center items-center word-spacing-wide text-black font-medium text-center   tracking-[.1em] font-outfit'
+		>
+			<div className='max-w-[1000px] mb-[-10px] rounded-md max-h-[480px] flex items-center bg-[#454545]  bg-opacity-80 m-4 flex-col md:flex-row justify-center'>
+				<div className='md:rounded-e-full max-w-[300px] flex max-h-[300px] w-full justify-center items-center text-2xl md:mr-4 bg-black overflow-hidden'>
+					<img
+						src={myPhoto}
+						className='max-w-full max-h-full object-center'
+						alt='lourdu-radjou-photo'
+					/>
+				</div>
+
+				<div className='md:border-l-4 border-t-4 md:border-t-0 border-[#ffffff] h-full w-full py-2 md:py-20 md:text-2xl text-lg mx-auto px-8 '>
+					{data.about2.split(',').map((line, index) => (
+						<p
+							key={index}
+							className='text-center border-b-2 border-b-red-400 mb-4'
+						>
+							{line}
+						</p>
+					))}
+				</div>
 			</div>
 		</div>
 	)
